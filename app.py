89f982c4f15d7149132d8627427e02d6ccf8b1dc
Carl Robinson <carl-robinson@users.noreply.github.com>
2019-03-03 18:40:56 +0100

    added ep022

--- app.py
+++ app.py
@@ -26,7 +26,9 @@ episodes = [
     {'num': '18', 'title': 'Prototypes and Personas - Kasia Ryniak and Rafal Cymerys, Upside', 'mp3': 'https://www.buzzsprout.com/159584/910239-prototypes-personas-kasia-ryniak-rafal-cymerys-upside-voice-tech-podcast-ep-018.mp3'},
     {'num': '19', 'title': 'Elevated Voice Design - Vasili Shynkarenka, Invocable', 'mp3': 'https://www.buzzsprout.com/159584/926167-elevated-voice-design-vasili-shynkarenka-invocable-voice-tech-podcast-ep-019.mp3'},
     {'num': '20', 'title': 'Machine Learning Signals - Christopher Oates, audEERING', 'mp3': 'https://www.buzzsprout.com/159584/942070-machine-learning-signals-christopher-oates-audeering-voice-tech-podcast-ep-020.mp3'},
-    {'num': '21', 'title': 'Nanocasting - Sean Gilligan, Sound Branch', 'mp3': 'https://www.buzzsprout.com/159584/959644-nanocasting-sean-gilligan-sound-branch-voice-tech-podcast-ep-021.mp3'}
+    {'num': '21', 'title': 'Nanocasting - Sean Gilligan, Sound Branch', 'mp3': 'https://www.buzzsprout.com/159584/959644-nanocasting-sean-gilligan-sound-branch-voice-tech-podcast-ep-021.mp3'},
+    {'num': '22', 'title': 'Voice Retail - Shilp Agarwal, Blutag', 'mp3': 'https://www.buzzsprout.com/159584/979019-voice-retail-shilp-agarwal-blutag-voice-tech-podcast-ep-022.mp3'}
+
 ]
 
 # ######################################
